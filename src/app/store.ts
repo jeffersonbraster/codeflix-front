70b08c7445c690999268c2bf47ddc84a2e3e3ed6
Jefferson <jeferson.oliveira@g.globo>
2022-08-29 19:40:26 -0300

    criando slice inicial de categorias

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,9 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import categoriesReducer from "../features/categories/categorySlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    categories: categoriesReducer,
   },
 });
 
---
+++ src/features/categories/categorySlice.ts
@@ -0,0 +1,39 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface Category {
+  id: string;
+  name: string;
+  description: null | string;
+  is_active: boolean;
+  deleted_at: null | string;
+  created_at: string;
+  updated_at: string;
+}
+const category: Category = {
+  id: "1",
+  name: "jeje",
+  description: "inital state",
+  is_active: true,
+  deleted_at: null,
+  created_at: "2021-01-01T00:00:00.000Z",
+  updated_at: "2021-01-01T00:00:00.000Z",
+};
+
+export const initalState = [
+  category,
+  { ...category, id: "2", name: "jeje2" },
+  { ...category, id: "3", name: "jeje3" },
+  { ...category, id: "4", name: "jeje4" },
+];
+
+const categoriesSlice = createSlice({
+  name: "categories",
+  initialState: initalState,
+  reducers: {
+    addCategory: (state, action) => {},
+    removeCategory: (state, action) => {},
+    updateCategory: (state, action) => {},
+  },
+});
+
+export default categoriesSlice.reducer;
